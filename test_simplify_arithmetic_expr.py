d3a69fed2447309d764bc3fef32ab846082bd054
Yaroslav Benin <dondublon1@yandex.ru>
2026-04-16 14:26:31 +0300

    tests for atirhmetic expressions regex

--- test_simplify_arithmetic_expr.py
+++ test_simplify_arithmetic_expr.py
@@ -3,12 +3,23 @@ import regular_expressions
 from unittest import TestCase
 
 class TestSimplifyArithmeticExpr(TestCase):
-    def test_simplify_arithmetic_expr(self):
+    def test_true_cases(self):
         simplifyArithmeticExprRX = regular_expressions.arithmeticRegex()
         self.assertTrue(re.fullmatch(simplifyArithmeticExprRX, "1+2"))
         self.assertTrue(re.fullmatch(simplifyArithmeticExprRX, "3-4"))
         self.assertTrue(re.fullmatch(simplifyArithmeticExprRX, "5*6"))
         self.assertTrue(re.fullmatch(simplifyArithmeticExprRX, "7/8"))
-        #self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "9+10-11"))
-        #self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "12*13/14"))
-        #self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "15+16*17"))
+        self.assertTrue(re.fullmatch(simplifyArithmeticExprRX, "9+10-11"))
+        self.assertTrue(re.fullmatch(simplifyArithmeticExprRX, "12*13/14"))
+        self.assertTrue(re.fullmatch(simplifyArithmeticExprRX, "15+16*17"))
+
+    def test_false_cases(self):
+        simplifyArithmeticExprRX = regular_expressions.arithmeticRegex()
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "+1"))
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "2-"))
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "*3"))
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "/4"))
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "5++6"))
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "7--8"))
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "9**10"))
+        self.assertFalse(re.fullmatch(simplifyArithmeticExprRX, "11//12"))
