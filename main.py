dfeb2ca95291484b9340d569a4b4e4b1a4ccd757
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-16 11:33:37 +0300

    no csasptured groups for IP address example

--- main.py
+++ main.py
@@ -6,15 +6,5 @@ DB Backup server has private IP 172.10.30.40
 Watch-dog monitoring server has private IP 172.10.88.15
 '''
 ip_pattern = re.compile(regular_expressions.ipV4AddressRegex())
-matchObj: re.Match[str] = ip_pattern.search(text)
-if( matchObj):
-    print("public IP of the server is ",matchObj.group())
-    print("first octet of the public IP is ", matchObj.group(1))
-    print("all separate octets of the public Ip is ", list(matchObj.groups())[:-1])
-backupIndex = text.index("Backup")
-matchObj = ip_pattern.search(text, backupIndex) 
-if (matchObj):
-    print("private IP of the Backup server is ",matchObj.group())
-    print("first octet of the private IP of the Backup server is ", matchObj.group(1))
-    print("all separate octets of the private IP of the Backup server is ", list(matchObj.groups())[:-1])
-      
+matches = ip_pattern.findall(text)
+print(matches)
