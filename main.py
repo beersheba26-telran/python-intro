e3eb54f3f86d4ef783a8b08cf8dfe9079f6b505e
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-16 12:18:53 +0300

    getting all IP addresses using finditer instead of findall to save memory

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import re
 import regular_expressions
+import sys
 text:str = '''
 Service is running on the server with public IP 54.10.8.33, DB server has private IP 172.10.15.20,
 DB Backup server has private IP 172.10.30.40
 Watch-dog monitoring server has private IP 172.10.88.15
 '''
 ip_pattern = re.compile(regular_expressions.ipV4AddressRegex())
-matches = ip_pattern.findall(text) #list of tuples with groups
-#FIXME
-allIpAddresses: list[str] = [".".join(tpl) for tpl in matches]
+matchObjectsIter = ip_pattern.finditer(text) #iterator of groups
+print("size of iterator", sys.getsizeof(matchObjectsIter))
+allIpAddresses: list[str] = [mo.group() for mo in matchObjectsIter]
 print(allIpAddresses)
+print("size of list is ", sys.getsizeof(allIpAddresses))
+
