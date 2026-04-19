6544f4aae6da4d6da23363cbb445e4864c442c3a
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-19 14:54:13 +0300

    multiplication and division evaluation without parentheses

--- arithmetic_evaluation.py
+++ arithmetic_evaluation.py
@@ -3,8 +3,10 @@ import operator
 import re
 import regular_expressions
 from arithmetic_check import checkArithmeticExpr
+numberRX: str = regular_expressions.numberRegex()
 operandsDelimPattern: re.Pattern = re.compile(regular_expressions.operatorRegex())
-operatorsDelimPattern: re.Pattern = re.compile(regular_expressions.numberRegex())
+operatorsDelimPattern: re.Pattern = re.compile(numberRX)
+mulDivExpPattern: re.Pattern = re.compile(rf"({numberRX})([*/])({numberRX})")
 ops: dict = {
     "+": operator.add,
     "-": operator.sub,
@@ -24,16 +26,24 @@ def __binEval(op1: float, op2: float, operation: str)->float:
         return ops[operation](op1, op2)
     except KeyError as e:
         raise ValueError(f"operation {operation} not implemented yet")
-    
+def __getExprWithNoMulDiv(expr: str)->str: 
+    while mo:=mulDivExpPattern.search(expr):
+        op1: float = float(mo.group(1))
+        op2: float = float(mo.group(3))
+        operation: float = mo.group(2)
+        value: float = __binEval(op1, op2 , operation)
+        expr = expr[:mo.start()] + str(value) + expr[mo.end():] 
+    return expr    
 def evaluation(expr: str)->float:
    '''
    evaluation arithmetic expression with assumption:
-   no multiplication, division, parentheses
+   no parentheses
    '''
-    
+   
    if not checkArithmeticExpr(expr):
        raise ValueError(f"syntax error in expression {expr}")
    expr = re.sub("\s+","",expr) #removing white symbols
+   expr = __getExprWithNoMulDiv(expr)
    operands: list[str] = __getOperands(expr)
    operators: list[str] = __getOperators(expr) #first operator is empty string
    result: float = float(operands[0])
--- test_mul_div_no_parentheses.py
+++ test_mul_div_no_parentheses.py
@@ -10,7 +10,9 @@ class TestArithmeticNMulDivNoParenthesesEval(TestCase):
     def test_right_expr_no_prefernces(self):
         expr: str = "2.23 *4 / 3.12 * 10 / 2* 100.54 -20 +40"
         self.assertAlmostEqual(2.23 *4 / 3.12 * 10 / 2* 100.54 -20 +40, evaluation(expr), places=2)
-        
+    def test_right_expr_preferences(self):
+        expr: str = "2.23 +4 / 3.12 * 10 / 2* 100.54 -20 +40" 
+        self.assertAlmostEqual(2.23 +4 / 3.12 * 10 / 2* 100.54 -20 +40, evaluation(expr), places=2)  
     def test_wrong_expr(self):
         with self.assertRaises(ValueError):
             evaluation("2,23 + 20")
