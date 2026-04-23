699477b2ccac5aaddcffdd8ea53ee029dd7025a3
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-23 11:55:55 +0300

    solution of the task "Count occurrences of strings in a list"

--- count_occurrences.py
+++ count_occurrences.py
@@ -6,5 +6,15 @@ def count_occurrences(strings: list[str]) -> list[tuple[str, int]]:
     1-st item of a tuple is a string
     2-st item of a tuple is the count of occurrences
     '''
-    #TODO
+    occurrences: dict = __getOccurrences(strings)
+    items = occurrences.items()
+    result:list[tuple[str, int]] = sorted(items, key=lambda item: (-item[1],item[0]))
+    return result
+def __getOccurrences(strings: list[str]) -> dict:
+    result: dict = {}
+    for string in strings:
+        count = result.get(string, 0)
+        result[string] = count + 1
+    return result    
+    
     
