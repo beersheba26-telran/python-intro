c00bd6a88451e5912addb23fea45d9bae9d2baaf
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-26 13:41:28 +0300

    SortedSet example

--- sorted_list_example.py
+++
@@ -1,8 +0,0 @@
-from sortedcontainers import SortedList, SortedKeyList
-sortedList: SortedList = SortedList(["Apple", "apple", "Apricot"])
-print(sortedList) # SortedList(['Apple', 'Apricot', 'apple'])
-#no possibility apply case insensitive comparing for SortedList, but only for SortedKeyList
-sortedKeyList: SortedKeyList = SortedKeyList(["Apple", "apple", "Apricot"], key= str.lower)
-print([s for s in sortedKeyList]) # ['Apple', 'apple', 'Apricot']
-
-
