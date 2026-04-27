827af95114b1d7bb619fea7138f9eaf795ce198b
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-27 14:11:38 +0300

    lru_cache final solution

--- test_lru_cach.py
+++ test_lru_cach.py
@@ -12,7 +12,7 @@ class TestLruCache(TestCase):
         self.cache.add(5, 25)
         self.assertEqual(25, self.cache.access(5))
         with self.assertRaises(KeyError):
-            self.cach.access(1)
+            self.cache.access(1)
     def test_access_ordering(self):
         self.assertEqual(1, self.cache.access(1))
         self.assertEqual(400, self.cache.access(20))
@@ -21,8 +21,24 @@ class TestLruCache(TestCase):
         self.assertEqual(1, self.cache.access(1))
         self.assertEqual(400, self.cache.access(20))
         with self.assertRaises(KeyError):
-            self.cach.access(10)
-        
-         
+            self.cache.access(10)
+    def test_square_braces_access(self):
+        self.assertEqual(400, self.cache[20])   # access
+    def test_square_braces_add(self):
+        self.cache[5] = 25
+        with self.assertRaises(KeyError):
+            self.cache[1]
+    def test_square_braces_update(self):
+        self.cache[20] = 40
+        self.assertEqual(40, self.cache[20]) 
+        self.assertEqual(1, self.cache[1] )     
+    def test_iterable(self):
+        current: int = 0
+        for key in self.cache:
+            self.assertEqual(numbers[current], key) 
+            current += 1      
+    def test_clear(self) :
+        self.cache.clear()
+        self.assertEqual(0, len([n for n in self.cache]))    
               
            
